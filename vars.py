a5956286b9511a57b6ebb28f2273a4dd4c2d979a
Passsmpaas <lokeshsingh17628@gmail.com>
2025-09-06 09:02:38 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,22 +3,23 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "29223018"))
-API_HASH = environ.get("API_HASH", "25b493c4989d22d7f2482f752d3d99ee")
+API_ID = int(environ.get("API_ID", "22470912"))
+API_HASH = environ.get("API_HASH", "511be78079ed5d4bd4c967bc7b5ee023)
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "8167879352"))
+OWNER = int(environ.get("OWNER", "7678862761"))
 CREDIT = environ.get("CREDIT", "【『 『 STRANGER 』 』】")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8167879352, 6039166844, 7818565931, 6126688051, 7568561381, 5136387513, 1445364767').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '7678862761, 6039166844, 7818565931, 6126688051, 7568561381, 5136387513, 1445364767').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8167879352, 6039166844, 7818565931, 6126688051, 7568561381, 5136387513, 1445364767').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '7678862761, 6039166844, 7818565931, 6126688051, 7568561381, 5136387513, 1445364767').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
 
 
